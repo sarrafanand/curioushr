de7cc549359a586170c6da61cbf687fc6c470bde
Ashish <ashishrodio@gmail.com>
2020-09-23 16:40:24 +0530

    website meta updated

--- config/website.js
+++ config/website.js
@@ -1,28 +1,30 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteTitle: 'Curious HR', // Navigation and Site Title
+  siteTitleAlt:
+    'Decoding human resource, labour legislations, industrial relations', // Alternative Site title for SEO
+  siteTitleShort: 'Curious HR', // short_name for manifest
+  siteUrl: 'https://www.curiousHR.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
-  author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  siteDescription:
+    'Decoding human resource, labour legislations, industrial relations. Dive in to explore more HR topics',
+  author: 'Anand', // Author for schemaORGJSONLD
+  organization: '',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
-  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
+  userTwitter: '@anandsarraf7', // Twitter Username
+  ogSiteName: 'Curious HR', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: '',
 
   // Manifest and Progress color
-  themeColor: '#5348FF',
+  themeColor: '#de4463',
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
+  twitter: 'https://twitter.com/anandsarraf7/',
+  twitterHandle: '@anandsarraf7',
+  github: '',
   linkedin: '',
 }
--- src/lib/colors.js
+++ src/lib/colors.js
@@ -3,7 +3,7 @@ const colors = {
   white: '#fff',
   gray: '#fafafa',
   red: '#E74C3C',
-  blue: '#5348ff',
+  blue: '#de4463',
   green: '#29B573',
 }
 
